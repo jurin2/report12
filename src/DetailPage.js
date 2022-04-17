caca4e9d519e2d5bda785399298efc2565d3829f
jurin2 <89722981+jurin2@users.noreply.github.com>
2022-04-17 20:21:31 +0900

    Add files via upload

--- src/DetailPage.js
+++ src/DetailPage.js
@@ -1,7 +1,10 @@
 import {useParams} from 'react-router-dom';
+import { useHistory } from "react-router-dom";
+
 
 function DetailPage(props){
     let {id} = useParams();
+    let history = useHistory();
     return(
         <div key={props.id} className="col-md-4">
           <img src={`https://jurin2.github.io/report12/src/images/img${
@@ -9,6 +12,9 @@ function DetailPage(props){
             }.jpg`} style={{width:'800px'}}/>
           <h2>{props.blogList[id].title}</h2>
           <p>{props.blogList[id].content}</p>
+          <button onClick={()=>{
+            history.go(-1)
+          }}>뒤로가기</button>
         </div>
     );
 }
